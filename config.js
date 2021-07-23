14e7e2fb9e9eb6b151397c674185a5d06b3bdec0
namtranbui2708 <87865860+namtranbui2708@users.noreply.github.com>
2021-07-23 21:59:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Xin chào em, người xinh nhất trong mắt anh',
+    introDesc: `Em hãy thật lòng trl
+    Câu hỏi sau đây của anh nhé
+    Đừng thoát mất công anh làm 
+    Dành riêng cho em người anh yêu`,
+    btnIntro: 'Vâng ạ',
+    title: 'Khi anh gửi cho em cái này cũng là lúc anh biết mình đã có tcam với em 😙',
+    desc: 'Trả lời anh đi , em có thích anh không vậy =)) ',
+    btnYes: 'Vẫn cứ là thích anh <3',
+    btnNo: 'Không, em ghét anhh :3',
+    question:'Anh có thể biết vì sao em thích anh không? ',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Vì anh có một đôi mắt đẹp ,
+    mess: 'Anh biết mà 🥰. Vì trong mắt anh luôn có em đó,
+    messDesc: 'Câu hỏi đến đây là hết rùi =)) nhắn tin lại cho anh để anh biết em đã trl hết nha ^^',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/namalabui2708 //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
